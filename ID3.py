083461f3c89ba14c6a2db5cd2250a85c8ead7ef1
Cracop <rodrigoalejandrobarrera@gmail.com>
2021-05-01 14:39:32 -0500

    Se añadió un dataset para crear el árbol y se hizo la clase de nodo

--- ID3.py
+++ ID3.py
@@ -1,63 +1,56 @@
 import pandas as pd
 import numpy as np
-import itertools
+import itertools 
 import math
 
-ycol = 'prognosis'
-dftrain = pd.read_csv('Testing.csv')
+ycol = 'lluvia'
+dftrain = pd.read_csv('prueba.csv')
 y_train = dftrain[ycol] 
 
-valores = y_train.unique()
+valores = y_train.unique() 
 #print(dftrain.head())
+
+class Nodo:
+    def __init__(self, nombre, padre, arista=None):
+        self.nombre=nombre
+        self.padre=padre
+        self.hijos = []
+        self.arista=arista #Es como der e izq en un bst
+        #Una arista es por donde llego al nodo, tipo trie
+
+    def addHijo(self, hijo):
+        self.hijos.append(hijo)
+
+    def buscaHijo(self, arista): #Si lle
+        for hijo in self.hijos:
+            if hijo.arista==arista:
+                return hijo
+        return None 
+
+
 
 def entropia(atributo):
     entropia = 0
+   
     for valor in dftrain[atributo].unique():
+        
         filasQueImportan = dftrain[dftrain[atributo]==valor] 
         n = 0
         print(len(filasQueImportan.index), "/", dftrain[atributo].size)
         proba1 = len(filasQueImportan.index)/dftrain[atributo].size
+        
         for resultado in filasQueImportan[ycol].unique():
+        
             resultadosQueImportan = filasQueImportan[filasQueImportan[ycol]==resultado]
             print("  ", resultado, len(resultadosQueImportan.index), "/", len(filasQueImportan.index))
             proba2 = len(resultadosQueImportan.index)/len(filasQueImportan.index)
             n += proba2*math.log2(proba2)
-        entropia += proba1*(-1*n)
+        
+        entropia += proba1*(-1)*(n)
     print(atributo, entropia)
 
-        
-        
-        
-
-
-
-entropia('itching')
-
-    
-
-"""
-
-
-
-  def entropy(self, attribute_column):
-    # find unique values and their frequency counts for the given attribute
-    values, counts = np.unique(attribute_column, return_counts=True)
-
-    # calculate entropy for each unique value
-    entropy_list = []
-
-    for i in range(len(values)):
-      probability = counts[i]/np.sum(counts)
-      entropy_list.append(-probability*np.log2(probability))
-
-    # calculate sum of individual entropy values
-    total_entropy = np.sum(entropy_list)
-
-    return total_entropy
-
-class Nodo:
-    def __init__(self):
-        self.valor = None
-        self.sig = None
-        self.hijos = None
-        self.padre = None"""
+for atributo in dftrain.columns:
+    entropia(atributo)
+#entropia('anxiety')
+#entropiaDelDataSet("prognosis")
+#print(dftrain.columns)
